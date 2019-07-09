ba4944cda645c91b92a0e977f3a42ff09f8555ff
github username <github email address>
2019-07-09 11:43:22 -0400

    added user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    before_action :authenticate!, except: [:index, :show]
+
+    byebug
+    private
+
+    def user_params
+        params.require[:user].permit[:name, :username, :password, :location, :avatar, :cover, :about, :followers_count, :following_count, :chirps_count, :created_at, :updated_at]
+    end
+
+    def check_user
+        if @user != current_user
+            redirect_to users_path
+        end 
+    end
+
+    def set_user
+        @user = User.friendly.find(params[:id])
+    end
 end
